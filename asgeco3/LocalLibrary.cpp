fd76ba45b8e6243cada13befb11ce7e72605b8cc
Antonio Magni <afm@toekneepro0.ciapee.net>
2013-02-09 18:40:04 +0100

    Initial Commit

---
+++ asgeco3/LocalLibrary.cpp
@@ -0,0 +1,30 @@
+//
+// LocalLibrary.cpp 
+// Library C++ code
+// ----------------------------------
+// Developed with embedXcode 
+// http://embedXcode.weebly.com
+//
+// Project asgeco3
+//
+// Created by Antonio Magni, 09/02/13 18:40
+// Antonio Magni
+//	
+//
+// Copyright © Antonio Magni, 2013
+// Licence CC = BY NC SA
+//
+// See LocalLibrary.cpp.h and ReadMe.txt for references
+//
+
+
+#include "LocalLibrary.h"
+
+void blink(uint8_t pin, uint8_t times, uint16_t ms) {
+  for (uint8_t i=0; i<times; i++) {
+    digitalWrite(pin, HIGH); 
+    delay(ms >> 1);               
+    digitalWrite(pin, LOW);  
+    delay(ms >> 1);              
+  }
+}
---
+++ asgeco3/ReadMe.txt
@@ -0,0 +1,24 @@
+
+  asgeco3
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project asgeco3
+  Created by Antonio Magni on 09/02/13
+  Copyright © 2013 Antonio Magni
+  Licence CC = BY SA NC
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  ----------------------------------
+  Embedded Computing Template on Xcode 4
+  Copyright © Rei VILO, 2010-2013
+  Licence CC = BY NC SA
+  http://embedXcode.weebly.com/
+
